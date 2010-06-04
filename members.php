feb37c05ad00d7e893a4777e0bc3c02a6598c3c7
Angel Pizarro <angel@upenn.edu>
2010-06-04 17:52:01 +0000

    added JH google cal to members page

--- members.php
+++ members.php
@@ -1,4 +1,4 @@
-<? 
+<?
     include 'includes.php';;
     phead('Lab Members');
     ptabs();
@@ -11,17 +11,25 @@
       <strong>John Hogenesch</strong>
       <br/>Associate Professor of <a href="http://www.med.upenn.edu/pharm/" > Pharmacology</a>
       <br/>Associate Director of the <a href="http://www.genomics.upenn.edu/"> Penn Genome Frontiers Institute</a>
-      <br/><img class='mail' src="images/jhogenesch.jpg" />		<br />
+      <br/><img class='mail' src="images/jhogenesch.jpg" />    <br />
       <p>
-<object width="230" height="85"><param name="movie" 
-value="https://clients4.google.com/voice/embed/webCallButton"></param><param 
-name="wmode" value="transparent"></param><param name="FlashVars" 
-value="id=a11986f986d3f21a7a4eedb07de987c2ffcd65e3&style=0"></param><embed 
-src="https://clients4.google.com/voice/embed/webCallButton" 
-type="application/x-shockwave-flash"wmode="transparent" width="230" 
-height="85" 
+        <div>
+          <div>
+<object width="230" height="85"><param name="movie"
+value="https://clients4.google.com/voice/embed/webCallButton"></param><param
+name="wmode" value="transparent"></param><param name="FlashVars"
+value="id=a11986f986d3f21a7a4eedb07de987c2ffcd65e3&style=0"></param><embed
+src="https://clients4.google.com/voice/embed/webCallButton"
+type="application/x-shockwave-flash"wmode="transparent" width="230"
+height="85"
 FlashVars="id=a11986f986d3f21a7a4eedb07de987c2ffcd65e3&style=0"></embed></object>
       </p>
+        </div>
+        <div>
+
+<iframe src="https://www.google.com/calendar/embed?title=John%20Hogenesch&amp;showPrint=0&amp;showCalendars=0&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=hogenesch%40gmail.com&amp;color=%23060D5E&amp;ctz=America%2FNew_York" style=" border-width:0 " width="700" height="400" frameborder="0" scrolling="no"></iframe>
+        </div>
+      </div>
     </div>
     <h3> Current Laboratory Members </h3>
     <div class="member">
@@ -33,7 +41,7 @@ FlashVars="id=a11986f986d3f21a7a4eedb07de987c2ffcd65e3&style=0"></embed></object
       <strong>Jason DeBruyne</strong>
       <br/>Senior Research Investigator
       <br/><img class='mail' src="images/jdebruyne.jpg" />
-    </div>    
+    </div>
     <div class="member">
       <strong>Jeanne Geskes</strong>
       <br/>Research Specialist
@@ -63,27 +71,27 @@ FlashVars="id=a11986f986d3f21a7a4eedb07de987c2ffcd65e3&style=0"></embed></object
   <div id="alumni">
     <h3> Alumni and Mentored Scientists </h3>
     <div class="member">
-      Pedro Aza-Blanc 
+      Pedro Aza-Blanc
       <br/>Director, Functional Genomics Resources, the <a href ='http://www.burnham.org/'>Burnham Institute</a>
       <br/><img class='mail' src="images/pazablanc.jpg" />
     </div>
     <div class="member">
-      Sumit Chanda 
+      Sumit Chanda
       <br/>Associate Professor, the <a href ='http://www.burnham.org/'>Burnham Institute</a>
       <br/><img class='mail' src="images/schanda.jpg" />
     </div>
     <div class="member">
-      Mike Conkright 
+      Mike Conkright
       <br/>Assistant Professor, the <a href ='http://www.scripps.edu/e_index.html'>Scripps Research Institute</a>
       <br/><img class='mail' src="images/mconkright.jpg" />
     </div>
     <div class="member">
-      Josephine Harada 
+      Josephine Harada
       <br/>Senior Manager, Business Development, <a href ='http://www.pulmatrix.com/' > Pulmatrix Incorporated</a>
       <br/><img class='mail' src="images/jharada.jpg" />
     </div>
     <div class="member">
-      Kevin Hayes 
+      Kevin Hayes
       <br/>Bioinformatics Research Scientist, <a href ='http://www.pioneer.com/web/site/portal/' >Pioneer Hybrid</a>
       <br/><img class='mail' src="images/khayes.jpg" />
     </div>
@@ -93,52 +101,52 @@ FlashVars="id=a11986f986d3f21a7a4eedb07de987c2ffcd65e3&style=0"></embed></object
       <br/><img class='mail' src="images/ines.jpg" />
     </div>
     <div class="member">
-      Loren Miraglia 
+      Loren Miraglia
       <br/>Research Investigator, the <a href ='http://www.gnf.org/' >Genomics Institute of the Novartis Research Foundation</a>
       <br/><img class='mail' src="images/lmiraglia.jpg" />
     </div>
     <div class="member">
-    Kinnery Naik, Public Health Prevention Specialist 
+    Kinnery Naik, Public Health Prevention Specialist
     <br/><a href="http://www.cdc.gov/"> Centers for Disease Control and Prevention</a>
     <br/><img class='mail' src="images/knaik.jpg" />
     </div>
     <div class="member">
-      Tony Orth 
+      Tony Orth
       <br/>Associate Director of Genomics, the <a href ='http://www.gnf.org/' >Genomics Institute of the Novartis Research Foundation</a>
       <br/><img class='mail' src="images/torth.jpg" />
     </div>
     <div class="member">
-      Satchin Panda 
+      Satchin Panda
       <br/>Assistant Professor, the <a href ='http://www.salk.edu/index.php' > Salk Institute for Biological Studies</a>
       <br/><img class='mail' src="images/spanda.jpg" />
     </div>
     <div class="member">
-      Susana Pires 
+      Susana Pires
       <br/>Graduate Student, <a href='http://www.ox.ac.uk/' > University of Oxford</a>
       <br/><img class='mail' src="images/susana.jpg" />
     </div>
     <div class="member">
-      Trey Sato 
+      Trey Sato
       <br/>Associate Scientist, <a href ="http://www.greatlakesbioenergy.org/"> Great Lakes Bioenergy Research Center</a>, <a href ='http://www.wisc.edu/' >University of Wisconsin</a>
       <br/><img class='mail' src="images/tsato.jpg" />
     </div>
     <div class="member">
-      Andrew Su 
+      Andrew Su
       <br/>Senior Research Investigator, the <a href ='http://www.gnf.org/' >Genomics Institute of the Novartis Research Foundation</a>
       <br/><img class='mail' src="images/asu.jpg" />
     </div>
     <div class="member">
-      John Walker 
+      John Walker
       <br/>Group Leader, the <a href ='http://www.gnf.org/' >Genomics Institute of the Novartis Research Foundation</a>
       <br/><img class='mail' src="images/jwalker.jpg" />
     </div>
     <div class="member">
-      Aaron Willingham 
+      Aaron Willingham
       <br/>Senior Scientist, <a href ='http://www.merck.com/' >Merck</a>
       <br/><img class='mail' src="images/awillingham.jpg" />
     </div>
     <div class="member">
-      Gina Zastrow 
+      Gina Zastrow
       <br/>Senior Research Associate, <a href='http://www.pioneer.com/web/site/portal/' >Pioneer Hybrid</a>
       <br/><img class='mail' src="images/gzastrow.jpg" />
     </div>
